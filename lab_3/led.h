42dcb148bf3c55faaab2a94cb4bb5e692c720cb8
Dangovsky <tenkovdaniil@gmail.com>
2019-03-20 15:57:43 +0300

    added on and color command

---
+++ lab_3/led.h
@@ -0,0 +1,15 @@
+#ifndef LIBLED_H 
+#define LIBLED_H 
+
+enum commands
+{
+    ON,
+    OFF,
+    TOGGLE,
+    BRIGHTNESS_UP,
+    BRIGHTNESS_DOWN,
+    BRIGHTNESS,
+    COLOR
+};
+
+#endif  /* LIBLED_H */
