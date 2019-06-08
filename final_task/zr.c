3bb868085ff28937b47bae2ead490325b765595f
Dangovsky <tenkovdaniil@gmail.com>
2019-06-08 22:45:46 +0300

    add router for tests

---
+++ final_task/zr.c
@@ -0,0 +1,63 @@
+#include "zb_common.h"
+#include "zb_scheduler.h"
+#include "zb_bufpool.h"
+#include "zb_nwk.h"
+#include "zb_aps.h"
+#include "zb_zdo.h"
+ 
+zb_ieee_addr_t g_zr_addr = {0x01, 0xbb, 0xbb, 0xbb, 0xbb, 0xbb, 0xbb, 0xbb};
+
+MAIN()
+{
+  ARGV_UNUSED;
+
+#if !(defined KEIL || defined SDCC|| defined ZB_IAR)
+  if ( argc < 3 )
+  {
+    //printf("%s <read pipe path> <write pipe path>\n", argv[0]);
+    return 0;
+  }
+#endif
+
+  /* Init device, load IB values from nvram or set it to default */
+#ifndef ZB8051
+  ZB_INIT("zdo_zr", argv[1], argv[2]);
+#else
+  ZB_INIT("zdo_zr", "2", "2");
+#endif
+#ifdef ZB_SECURITY
+  ZG->nwk.nib.security_level = 0;
+#endif
+
+  ZB_AIB().aps_channel_mask = (1l << 19); 
+  ZB_IEEE_ADDR_COPY(ZB_PIB_EXTENDED_ADDRESS(), &g_zr_addr); 
+
+  if (zdo_dev_start() != RET_OK)
+  {
+    TRACE_MSG(TRACE_ERROR, "zdo_dev_start failed", (FMT__0));
+  }
+  else
+  {
+    zdo_main_loop();
+  }
+
+  TRACE_DEINIT();
+
+  MAIN_RETURN(0);
+}
+
+
+void zb_zdo_startup_complete(zb_uint8_t param) ZB_CALLBACK
+{
+  zb_buf_t *buf = ZB_BUF_FROM_REF(param);
+  if (buf->u.hdr.status == 0)
+  {
+    TRACE_MSG(TRACE_APS1, "Device STARTED OK", (FMT__0));
+  }
+  else
+  {
+    TRACE_MSG(TRACE_ERROR, "Device started FAILED status %d", (FMT__D, (int)buf->u.hdr.status));
+    zb_free_buf(buf);
+  }
+}
+
